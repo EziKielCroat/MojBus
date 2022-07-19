17c347d591d68ec1732938a7a1e94d9d476d1a7f
EziKielCroat <89482125+EziKielCroat@users.noreply.github.com>
2022-07-19 23:43:15 +0200

    Finalising project

--- client.js
+++ client.js
@@ -1,21 +1,18 @@
 let put = document.getElementById("put-btn");
 let mjesto = document.getElementById("mjesto-btn");
 let bus = document.getElementById("bus-btn");
-document.getElementById("bus-select").value = "";
 let valueChanged = false;
 
+
+document.getElementById("bus-select").value = "";
 
     require([
         "esri/config",
         "esri/Map",
-        "esri/views/MapView",
-        "esri/Graphic",
-        "esri/rest/route",
-        "esri/rest/support/RouteParameters",
-        "esri/rest/support/FeatureSet"
-      ], function(esriConfig, Map, MapView, Graphic, route, RouteParameters, FeatureSet) {
+        "esri/views/MapView"
+      ], function(esriConfig, Map, MapView) {
   
-      esriConfig.apiKey = "YOUR-API-KEY"; // Replace this
+      esriConfig.apiKey = "AAPKbe5eff22a2a948948d6e3187fdd67d6fKKYW43E8DoLoevr7zIJfInnNZbDIvukXwVWOKALJDtgpR1dInnoheujWTTBv8xSG"; // Replace this
   
       const map = new Map({
         basemap: "arcgis-navigation"
@@ -26,13 +23,13 @@ let valueChanged = false;
         map: map,
         center: [16.465, 43.51], // X, Y
         zoom: 12,
-      });
+      }); // leave for now, will remove later 
 
         put.addEventListener("click", function() {
             valueChanged = true;
             setTimeout(function(){
              valueChanged = false;
-             confirm("Odaberite početak i destinaciju"); // vjerovatno promjenit u zasebni styled window?
+             confirm("Odaberite početak i destinaciju"); // vjerovatno promjenit u zasebni styled window? // razmislit o ovome napravit
           }, 1250);
             putBtnHandler(valueChanged);
             menuOnClick();
